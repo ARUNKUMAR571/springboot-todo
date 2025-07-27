cbf46dc1022820bfb8ff1be6e8beec4652128b35
ARUNKUMAR571 <arunkumar.sodad571@gmail.com>
2025-07-27 09:04:38 +0530

    initial commit

---
+++ src/main/java/com/app/todoapp/models/Task.java
@@ -0,0 +1,40 @@
+package com.app.todoapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private boolean completed;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
